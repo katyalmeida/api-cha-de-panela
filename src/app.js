9531dbca7b22f20c78c4e78c70c2d7affb4e596d
katyalmeida <almeidakatiane29@gmail.com>
2024-09-11 21:18:58 -0300

    feat: creating app and server configurations

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import routes from "./routes.js";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import "./database/index.js";
+
+class App {
+  constructor() {
+    this.app = express();
+    this.app.use(cors());
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.json());
+    this.app.use(
+      "/products-file",
+      express.static(resolve(__dirname, "..", "uploads"))
+    );
+  }
+
+  routes() {
+    this.app.use(routes);
+  }
+}
+
+export default new App().app;
